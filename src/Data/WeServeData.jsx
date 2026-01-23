14def1e6b94ddd98b4f0e04ccd77952d60a02af5
vernoxy <vernoxy3@gmail.com>
2026-01-23 14:40:39 +0530

    Push Last Work on We Serve

---
+++ src/Data/WeServeData.jsx
@@ -0,0 +1,31 @@
+import img1 from "../assets/HomePageAssets/WeServe.png";
+import img2 from "../assets/HomePageAssets/WeServe.png";
+import img3 from "../assets/HomePageAssets/WeServe.png";
+import img4 from "../assets/HomePageAssets/WeServe.png";
+
+export const WeServeData = [
+    {
+        id: 1,
+        img: img1,
+        title: "Chemical & Petrochemical Industries",
+        text: "Protective coatings for reactors, pipelines, storage tanks, and process equipment exposed to aggressive chemicals.",
+    },
+    {
+        id: 2,
+        img: img2,
+        title: "Pharmaceutical Industries",
+        text: "Hygienic, corrosion-resistant coatings for cleanroom areas, production plants, and utility structures.",
+    },
+    {
+        id: 3,
+        img: img3,
+        title: "Chemical & Petrochemical Industries",
+        text: "Protective coatings for reactors, pipelines, storage tanks, and process equipment exposed to aggressive chemicals.",
+    },
+    {
+        id: 4,
+        img: img4,
+        title: "Pharmaceutical Industries",
+        text: "Hygienic, corrosion-resistant coatings for cleanroom areas, production plants, and utility structures.",
+    }
+]
--- src/Pages/Homepage/HomePageSections/IndustriesWeServe.jsx
+++ src/Pages/Homepage/HomePageSections/IndustriesWeServe.jsx
@@ -1,24 +1,52 @@
 import React from "react";
-import IndustriesCard from "../../../Components/UI/IndustriesCard"
+import IndustriesCard from "../../../Components/UI/IndustriesCard";
+import { WeServeData } from "../../../Data/WeServeData";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+
 
 const IndustriesWeServe = () => {
   return (
     <section className="container">
       <p className="uppercase text-xl font-bold text-primary">
-       [ Industries We Serve ]
+        [ Industries We Serve ]
       </p>
       <hr className="bg-black" />
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap gap-y-4">
         <div className="md:w-1/2">
           <h1 className="text-textcolor">We serve the following industries</h1>
         </div>
-        <div className="md:w-1/2">
-            <p>AMICOL provides high-performance industrial paints and coating solutions tailored to the specific needs of diverse industries. Our products are designed to deliver long-term protection, corrosion resistance, and durability in demanding operating environments.</p>
+        <div className="md:w-1/2 flex">
+          <div className="h-full rounded-full px-0.5  bg-primary mr-6"></div>
+          <div className="md:flex space-y-4 justify-between">
+            <p className="text-textcolor md:max-w-[75%] max-w-[90%]">
+              AMICOL provides high-performance industrial paints and coating
+              solutions tailored to the specific needs of diverse industries.
+              Our products are designed to deliver long-term protection,
+              corrosion resistance, and durability in demanding operating
+              environments.
+            </p>
+            <div className=" flex gap-2.5">
+              <div className="h-10 w-10 border-2 rounded-full flex items-center justify-center cursor-pointer hover:bg-primary text-gray-500 hover:border-primary hover:text-white duration-300 transition-colors">
+                <FaChevronLeft className="text-sm " />
+              </div>
+              <div className="h-10 w-10 border-2 rounded-full flex items-center justify-center cursor-pointer hover:bg-primary text-gray-500 hover:border-primary hover:text-white duration-300 transition-colors">
+                <FaChevronRight className="text-sm " />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex overflow-auto gap-8">
-        <IndustriesCard />
-        {/* <IndustriesCard /> */}
+      {/* cards */}
+      <div className="flex flex-wrap overflow-auto gap-8 pt-12 w-full">
+        {WeServeData.map((data, index) => (
+          <IndustriesCard
+            Img={data.img}
+            id={data.id}
+            key={index}
+            text={data.text}
+            title={data.title}
+          />
+        ))}
       </div>
     </section>
   );
