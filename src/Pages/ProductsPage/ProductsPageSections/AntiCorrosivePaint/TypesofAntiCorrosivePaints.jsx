751ff2e8e2ffd02dfc7914490cb554d6c28a96a9
vernoxy <vernoxy3@gmail.com>
2026-01-24 17:26:50 +0530

    Footer lift up

--- src/Pages/ProductsPage/ProductsPageSections/AntiCorrosivePaint/TypesofAntiCorrosivePaints.jsx
+++ src/Pages/ProductsPage/ProductsPageSections/AntiCorrosivePaint/TypesofAntiCorrosivePaints.jsx
@@ -4,7 +4,7 @@ import { ProductCards } from "../../../../Components/UI/ProductCards";
 
 const TypesofAntiCorrosivePaints = () => {
   return (
-    <section className="bg-[#F4F4F4]  py-14 pb-44">
+    <section className="bg-[#F4F4F4]  py-14 md:pb-40 xl:pb-48">
       <h1 className="text-center">Types of Anti-Corrosive Paints</h1>
       <div className="container grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 pt-6">
         {TypesofAntiCorrosivePaintsData.map((item) => (
